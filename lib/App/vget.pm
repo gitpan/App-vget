a657904cb2668f8dc84e307793b5c92488b950df
Marcel Grünauer <marcel@cpan.org>
2007-11-08 12:31:30 +0000

    Import of MARCEL/App-vget-0.01 from CPAN.
    gitpan-cpan-distribution: App-vget
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         MARCEL/App-vget-0.01.tar.gz
    gitpan-cpan-author:       MARCEL
    gitpan-cpan-maturity:     released

---
+++ lib/App/vget.pm
@@ -0,0 +1,62 @@
+package App::vget;
+
+use warnings;
+use strict;
+
+
+our $VERSION = '0.01';
+
+
+1;
+
+
+__END__
+
+=head1 NAME
+
+App::vget - Marker package for vget
+
+=head1 SYNOPSIS
+
+None.
+
+=head1 DESCRIPTION
+
+This is just a marker package so you can install this distribution from the
+CPAN shell.
+
+=head1 TAGS
+
+If you talk about this module in blogs, on del.icio.us or anywhere else,
+please use the C<appvget> tag.
+
+=head1 BUGS AND LIMITATIONS
+
+No bugs have been reported.
+
+Please report any bugs or feature requests to C<bug-app-vget@rt.cpan.org>, or
+through the web interface at L<http://rt.cpan.org>.
+
+=head1 INSTALLATION
+
+See perlmodinstall for information and options on installing Perl modules.
+
+=head1 AVAILABILITY
+
+The latest version of this module is available from the Comprehensive Perl
+Archive Network (CPAN). Visit <http://www.perl.com/CPAN/> to find a CPAN
+site near you. Or see <http://www.perl.com/CPAN/authors/id/M/MA/MARCEL/>.
+
+=head1 AUTHOR
+
+Marcel GrE<uuml>nauer, C<< <marcel@cpan.org> >>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2004-2007 by Marcel GrE<uuml>nauer
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
